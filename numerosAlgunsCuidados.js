dd543a588b9220af83e688c118b3948f6179c9c8
luiz paulo <luizmxt@gmail.com>
2018-11-18 23:12:39 -0200

    Primeiro Update dos meus arquivos para o GitHub

---
+++ numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0)
+console.log("10" / 2)
+console.log("show!" * 2) //Não é um Número
+console.log(0.1 + 0.7) //Imprecisão
+// console.log(10.toString()) Não funciona
+console.log((10.455).toFixed(2))
