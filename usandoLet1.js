78ac1bfe6a0e6e432e7195913f5447e6abdabab9
luiz paulo <luizmxt@gmail.com>
2018-11-27 00:22:59 -0200

    Add os arquivos usando Var/Let 1 e 2 criando loop com Var e Let

---
+++ usandoLet1.js
@@ -0,0 +1,6 @@
+let numero = 1 //var ou let
+{
+    let numero = 2
+    console.log('Dentro =', numero)
+}
+console.log('Fora =', numero)
---
+++ usandoVar1.js
@@ -0,0 +1,16 @@
+{ 
+    {
+        {
+              var sera = "será?" 
+              console.log(sera) //padrao de outras linguagem 
+        }
+    } 
+}
+console.log(sera) // Os dois modelos funcionam no JS
+
+function teste(){
+    var local = 123
+    console.log(local)
+}
+teste()
+console.log(local)
