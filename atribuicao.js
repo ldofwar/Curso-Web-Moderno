af4bc7381e503a763ead0ff7507c307da71f18e4
luiz paulo <luizmxt@gmail.com>
2018-12-03 22:37:56 -0200

    Add arquivos atribuicao destructuring1

---
+++ atribuicao.js
@@ -0,0 +1,17 @@
+const a = 9
+let b = 2
+
+b += a // b = b + a
+console.log(b)
+
+b -= a // b = b - a
+console.log(b)
+
+b *= a // b = b * a
+console.log(b)
+
+b /= a // b = b / a
+console.log(b)
+
+b %= a // b = b % a
+console.log(b)
